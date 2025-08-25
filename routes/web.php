6977236d4734bf41b864b1865d9ad28bcb7959fd
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2025-08-25 17:18:34 +0700

    yeay

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,6 @@ Route::get('/dashboard', function () {
 
 
 
-
-
-// Profile routes (untuk semua user)
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
 require __DIR__ . '/settings.php';
 require __DIR__ . '/admin.php';
